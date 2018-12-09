9f7f619658d181fb02908f9dea28a9e5ae849b77
Huynh Quang-Nghi <hqnghi88@gmail.com>
2018-12-09 20:51:20 +0700

    maven deploy

--- publish.sh
+++ publish.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-bash ./pushToBintray.sh hqnghi88 $BINTRAY gama_genstar genstar.plugin.bundle-all.feature 1.0.0 /
+
+cd genstar.p2updatesite &&
+mvn -U clean install -P p2Repo --settings ../settings.xml && 
+cd - && bash ./pushToBintray.sh hqnghi88 $BINTRAY gama_genstar genstar.plugin.bundle-all.feature 1.0.0 genstar.p2updatesite\target\
