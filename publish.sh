65ea3804e612a6a73a878c6b8a1de598147954ab
Huynh Quang-Nghi <hqnghi88@gmail.com>
2018-12-09 21:08:58 +0700

    maven deploy

--- publish.sh
+++ publish.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bash ./pushToBintray.sh hqnghi88 $BINTRAY gama_genstar genstar.plugin.bundle-all.feature 1.0.0 genstar.p2updatesite\target\p2Repo
+bash ./pushToBintray.sh hqnghi88 $BINTRAY hqnghi88 gama_genstar "genstar.plugin.bundle-all.feature" "1.0.0" /home/travis/build/hqnghi88/gamaplugin/genstar.p2updatesite/target/p2Repo
